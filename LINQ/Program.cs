0ce8b55e8ed577f9ed470007795766cdd9efd5bf
Robert Montgomery <boobyvxvbooby_gmail.com>
2024-07-29 11:02:51 -0500

    Added few more games... honestly this list could be longer!

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace LINQ
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //This will be the list of my favorite games not in order of favoritism
+            List<string> videoGames = new List<string>
+            {
+                "Half-Life Series",
+                "The legend of zelda: A Link back to the past!",
+                "Minecraft",
+                "Dead by Daylight",
+                "Pal-World",
+                "Final Fantasy 14 Online",
+                "Guild Wars 2",
+                "Fallout series",
+                "DOOM"
+
+            };
+            
+            // This will order the length of the name of the game using the above LINQ method
+
+            var sortedVideoGames = videoGames
+                .OrderBy(game => game.Length)
+                .ToList();
+            
+            // This will sort list to the console
+
+            foreach (var game in sortedVideoGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
